edeaa750712b1a18ac6f930b762ce6a6bee4baa1
Oğulcan TÜRK <hi@ogulcan.dev>
2022-01-20 12:51:00 +0300

    auth and policies added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 // Amplify
 import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'bc7eb79a-d3b2-4589-a16d-41d3a5cd8dc3',
+    region: 'eu-central-1'
+  }
+});
 
 // Tailwind
 import './assets/tailwind.css'
