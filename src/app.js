96e7f6b3f3e22b8a69b5d4fe5dcfde44e706aacc
Oğulcan TÜRK <hi@ogulcan.dev>
2022-01-20 14:17:25 +0300

    added geofence and tracking demo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 // Amplify
 import Amplify from "aws-amplify";
-import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'eu-central-1:bc7eb79a-d3b2-4589-a16d-41d3a5cd8dc3',
+    region: 'eu-central-1',
+    userPoolId: 'eu-central-1_wD10zrgGo',
+    userPoolWebClientId: '7tajoukpihgse7pft406hhn7t1',
+  },
+  geo: {
+    AmazonLocationService: {
+      maps: {
+        items: {
+          "lsdemomap2-dev": {
+            style: "Esri Light",
+          },
+        },
+        default: "lsdemomap2-dev",
+      },
+      region: 'eu-central-1',
+    },
+  },
+})
 
 // Tailwind
 import './assets/tailwind.css'
--- src/views/home.js
+++ src/views/home.js
@@ -5,6 +5,10 @@ import {useRef, useEffect} from 'react';
 import { createMap } from "maplibre-gl-js-amplify";
 import "maplibre-gl/dist/maplibre-gl.css";
 
+// Location
+import {Auth} from 'aws-amplify';
+import * as AWS from "@aws-sdk/client-location";
+
 const Home = () => {
 
   const mapRef = useRef(null);
@@ -15,13 +19,45 @@ const Home = () => {
       if (mapRef.current != null) {
         map = await createMap({
           container: mapRef.current,
-          center: [-122.431297, 37.773972],
+          center: [29.027333812492884, 40.986657335813845],
           zoom: 13,
         });
       }
     }
     initializeMap();
   }, []);
+
+  useEffect(async () => {
+    // Set Client
+    const credentials = await Auth.currentCredentials();
+    const client = new AWS.Location({
+      credentials,
+      region: "eu-central-1"
+    });
+
+    // Route
+    const exampleRoute = [
+      [29.057074963280257, 40.96790426401776], // Caddebostan
+      [29.04382986814383, 40.97390463095644], // Fenerbahçe
+      [29.03617714651068, 40.98879207313138], // Söğütlüçeşme
+      [29.02543390267789, 40.98868098526151], // Çarşı
+      [29.00455815351762, 40.99730780737562] // Vapur
+    ]
+
+    for(let s=0; s<exampleRoute.length; s++) {
+      setTimeout(() => {
+        console.log(exampleRoute[s]);
+        client.batchUpdateDevicePosition({
+          TrackerName: 'lsdemotracker-dev',
+          Updates: [{
+            DeviceId: 'lsdemodevice',
+            Position: exampleRoute[s],
+            SampleTime: new Date()
+          }]
+        });
+      }, s * 5000);
+    }
+  },[]);
 
   return (
     <div className="bg-gray-50">
