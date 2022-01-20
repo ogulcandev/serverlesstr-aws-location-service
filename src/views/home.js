162ce88956e1f5f3073a41a4c2560624dc23bf87
Oğulcan TÜRK <hi@ogulcan.dev>
2022-01-20 15:23:23 +0300

    marker added and completed

--- src/views/home.js
+++ src/views/home.js
@@ -1,7 +1,8 @@
 // React
-import {useRef, useEffect} from 'react';
+import {useRef, useEffect, useState} from 'react';
 
 // Map
+import { Marker } from 'maplibre-gl';
 import { createMap } from "maplibre-gl-js-amplify";
 import "maplibre-gl/dist/maplibre-gl.css";
 
@@ -11,60 +12,61 @@ import * as AWS from "@aws-sdk/client-location";
 
 const Home = () => {
 
+  const credentials = Auth.currentCredentials();
+  const client = new AWS.Location({
+    credentials,
+    region: "eu-central-1"
+  });
+
+  const marker = new Marker();
+
   const mapRef = useRef(null);
+  const [currentLocation, setCurrentLocation] = useState([29.057074963280257, 40.96790426401776]);
+
+  function handleLocationChange() {
+    const {lng, lat} = marker.getLngLat();
+    console.log('Current location changed!', [lng, lat]);
+    setCurrentLocation([lng, lat]);
+  };
 
   useEffect(() => {
     async function initializeMap() {
-      //
       if (mapRef.current != null) {
         map = await createMap({
           container: mapRef.current,
           center: [29.027333812492884, 40.986657335813845],
-          zoom: 13,
+          zoom: 12,
         });
+        marker
+          .setLngLat(currentLocation)
+          .setDraggable(true)
+          .on('dragend', handleLocationChange)
+          .addTo(map)
       }
     }
     initializeMap();
   }, []);
 
   useEffect(async () => {
-    // Set Client
-    const credentials = await Auth.currentCredentials();
-    const client = new AWS.Location({
-      credentials,
-      region: "eu-central-1"
+    // Log
+    console.log('Current location', currentLocation);
+    // Send track data
+    client.batchUpdateDevicePosition({
+      TrackerName: 'lsdemotracker-dev',
+      Updates: [{
+        DeviceId: 'lsdemodevice',
+        Position: currentLocation,
+        SampleTime: new Date()
+      }]
     });
-
-    // Route
-    const exampleRoute = [
-      [29.057074963280257, 40.96790426401776], // Caddebostan
-      [29.04382986814383, 40.97390463095644], // Fenerbahçe
-      [29.03617714651068, 40.98879207313138], // Söğütlüçeşme
-      [29.02543390267789, 40.98868098526151], // Çarşı
-      [29.00455815351762, 40.99730780737562] // Vapur
-    ]
-
-    for(let s=0; s<exampleRoute.length; s++) {
-      setTimeout(() => {
-        console.log(exampleRoute[s]);
-        client.batchUpdateDevicePosition({
-          TrackerName: 'lsdemotracker-dev',
-          Updates: [{
-            DeviceId: 'lsdemodevice',
-            Position: exampleRoute[s],
-            SampleTime: new Date()
-          }]
-        });
-      }, s * 5000);
-    }
-  },[]);
+  },[currentLocation]);
 
   return (
     <div className="bg-gray-50">
       <div className="container w-full h-screen">
         <h1 className="text-gray-800 font-bold">AWS Location Service Demo</h1>
         <p className="text-gray-900 font-normal">React, TailwindCSS and Parcel</p>
-        <div ref={mapRef} id="map" className="w-[600px] h-[400px]" />
+        <div ref={mapRef} id="map" className="w-[600px] h-[400px]"/>
       </div>
     </div>
   )
